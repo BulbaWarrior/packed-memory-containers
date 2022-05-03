8fd1ce28a67b4a62494e2529bedf3fd6f70b76ae
Vladislav Kalmykov <vladkalm49@gmail.com>
2022-05-03 06:30:22 +0300

    some bad fusion examples

--- app/VectorUpdate.hs
+++ app/VectorUpdate.hs
@@ -6,7 +6,7 @@ module VectorUpdate where
 import qualified Data.Vector as V
 
 main :: IO ()
-main = fullFusion
+main = partialFusion
 
 fullFusion = do
   let v = V.fromList [1..10^5]
@@ -15,11 +15,35 @@ fullFusion = do
 
 partialFusion = do
   values <- map read . lines <$> getContents
-  let v = V.fromList [1..10^5]
+  let v = V.fromList [1..10^7]
       newV = v V.// [(0, x) | x <- values]
+  print $ newV V.! 9
+
+
+badFusion = do
+  values <- map read . lines <$> getContents
+  let v = V.fromList [1..10^7]
+      newV = v V.// [(0, x) | x <- values]
+      newV2 = newV V.// [(0, x) | x <- values]
+  print $ newV2 V.! 9
+
+
+badFusion2 = do
+  values <- map read . lines <$> getContents
+  let v = V.fromList [1..10^7]
+      newV = v V.// [(0, x) | x <- values] V.// [(0, x) | x <- values]
+  print $ newV V.! 9
+
+badFusion3 = do
+  values <- map read . lines <$> getContents
+  let v = V.fromList [1..10^7]
+      newV = v V.// (concat   [[(0, x) | x <- values], [(0, x) | x <- values]] )
   print $ newV V.! 9
 
 noFusion = do
   let v = V.fromList [1..10^5]
       newV = V.foldl (\acc x -> V.update acc $ V.singleton (0, x)) v $ V.fromList [1..10^5]
   print $ newV V.! 9
+
+simpleFusion = do
+  print $ (V.singleton 1 <> V.singleton 2) V.! 0
